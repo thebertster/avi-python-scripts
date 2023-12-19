086b348f44114c0eb3fc26eea1b4a78138d7bb02
Roberto Casula <rcasula@vmware.com>
2023-12-19 10:34:11 +0000

    Add object_to_hcl

--- README.md
+++ README.md
@@ -26,34 +26,6 @@ This will restore the specific VS called "rc-demo" in the tenant "demo-tenant" f
 
 `backup_restore.py -c <controller> -v rc-demo -t demo-tenant restore my_backup.json`
 
-## vrf_map.py
-
-Script to output the mapping between Avi VRF name and local Linux network namespace on a given Service Engine.
-
-## licenses.py
-
-Script to list and delete licenses from the Controller. This is particularly useful for deleting ENTERPRISE licenses (including evaluation licenses) that are still present in the system after the Controller has been switched to ENTERPRISE with CLOUD SERVICES tier.
-
-## vmac.py
-
-Script to calculate the Virtual MACs used for floating IPs in Network Services.
-
-*Example:*
-
-This will print out the VMAC corresponding to all Floating IPs defined in Network Service "example_ns" in tenant "example_tenant":
-
-`vmac.py -c <controller> -t example_tenant -n example_ns`
-
-## inventory_report.py
-
-This script uses the Inventory APIs to export summary information about VS, Pool or Service Engines to the screen in tabular form, or to a CSV file that can then be used for reporting purposes.
-
-*Example:*
-
-This will export the inventory of all pools in the tenant "example_tenant" as well as each pool's member servers to a CSV file "output.csv":
-
-`inventory_report.py -c <controller> -t example_tenant -i pooldetail -f output.csv`
-
 ## csv_metrics.py
 
 Exports specified Virtual Service or Service Engine metrics to the screen or to a simple CSV file for analysis, graphing etc (e.g. using Excel!). Also supports aggregated Virtual Service metrics at the SE level.
@@ -68,15 +40,41 @@ This will display the last minute's worth of real-time metrics for SSL handshake
 
 `csvmetrics.py -c <controller> -t example_tenant -vs * -se avi-se-example -m l7_client.avg_ssl_handshakes_new -g realtime -l 1m`
 
-## unused_objects.py
+## inventory_report.py
 
-Lists objects within the Controller that are not used or referenced by any other objects, with the option to delete such objects.
+This script uses the Inventory APIs to export summary information about VS, Pool or Service Engines to the screen in tabular form, or to a CSV file that can then be used for reporting purposes.
 
 *Example:*
 
-This will list all orphaned objects in any tenant of type Pool,PoolGroup,HTTPPolicySet or L4PolicySet and prompt the user whether to delete each orphaned object that is found:
+This will export the inventory of all pools in the tenant "example_tenant" as well as each pool's member servers to a CSV file "output.csv":
 
-`unused_objects.py -c <controller> -t * -o pool,poolgroup,httppolicyset,l4policyset -d`
+`inventory_report.py -c <controller> -t example_tenant -i pooldetail -f output.csv`
+
+## licenses.py
+
+Script to list and delete licenses from the Controller. This is particularly useful for deleting ENTERPRISE licenses (including evaluation licenses) that are still present in the system after the Controller has been switched to ENTERPRISE with CLOUD SERVICES tier.
+
+## object_to_hcl.py
+
+Script to generate Terraform HCL from an existing object or objects. When using Terraform for automation, rather than building the Terraform resource from scratch, it is often easier to create an example of the desired configuration via the UI and then export the configured object directly to Terraform HCL which can then be tweaked to create a templatized resource.
+
+Note 1: This script requires that the terraform executable is in the execution search path.
+
+Note 2: By default, the script will assume the version of the `vmware/avi` Terraform Provider should match the Avi REST API version being used. The Terraform Provider version can be specified explicitly using the `-tx` parameter if required.
+
+*Examples:*
+
+This will export all Virtual Services in the tenant "example_tenant" to a Terraform HCL file "example_vs.tf":
+
+`object_to_hcl.py -c <controller> -t example_tenant virtualservice example_vs.tf`
+
+This will export the Virtual Services named "example_vs1" and "example_vs2" in the tenant "example_tenant" to a Terraform HCL file "example_vs.tf":
+
+`object_to_hcl.py -c <controller> -t example_tenant virtualservice -n example_vs,example_vs2 example_vs.tf`
+
+This will export all Application Profiles whose names containthe string "System-" in the admin tenant to a Terraform HCL file "app_profiles.tf":
+
+`object_to_hcl.py -c <controller> applicationprofile -c System- app_profiles.tf`
 
 ## remove_ciphers.py
 
@@ -95,6 +93,16 @@ This will replace references to "System-Default-Cert" with "My-Wildcard-Cert" an
 `replace_certificates.py -c <controller> -t example_tenant System-Default-Cert,System-Default-Cert-EC My-Wildcard-Cert,My-Wildcard-Cert-EC`
 
 Note: When running in all tenants (`-t *`) where certificates of the same name may exist in multiple tenants, it may be necessary to specify the certificates using their UUIDs rather than their names.
+
+## unused_objects.py
+
+Lists objects within the Controller that are not used or referenced by any other objects, with the option to delete such objects.
+
+*Example:*
+
+This will list all orphaned objects in any tenant of type Pool,PoolGroup,HTTPPolicySet or L4PolicySet and prompt the user whether to delete each orphaned object that is found:
+
+`unused_objects.py -c <controller> -t * -o pool,poolgroup,httppolicyset,l4policyset -d`
 
 ## user_tokens.py
 
@@ -116,6 +124,20 @@ This will delete a specific token (obtain the token's UUID using the list operat
 This will create a new token for user "example-user" time-limited to 3 hours:
 
 `user_tokens.py -c <controller> create example-user -e 3`
+
+## vmac.py
+
+Script to calculate the Virtual MACs used for floating IPs in Network Services.
+
+*Example:*
+
+This will print out the VMAC corresponding to all Floating IPs defined in Network Service "example_ns" in tenant "example_tenant":
+
+`vmac.py -c <controller> -t example_tenant -n example_ns`
+
+## vrf_map.py
+
+Script to output the mapping between Avi VRF name and local Linux network namespace on a given Service Engine.
 
 ## waf_report.py
 
---
+++ object_to_hcl.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+import argparse
+import getpass
+
+import requests
+import urllib3
+from avi.sdk.avi_api import ApiSession
+from tempfile import TemporaryDirectory
+from subprocess import run
+
+# Disable certificate warnings
+
+if hasattr(requests.packages.urllib3, 'disable_warnings'):
+    requests.packages.urllib3.disable_warnings()
+
+if hasattr(urllib3, 'disable_warnings'):
+    urllib3.disable_warnings()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-c', '--controller',
+                        help='FQDN or IP address of NSX ALB Controller')
+    parser.add_argument('-u', '--user', help='NSX ALB API Username',
+                        default='admin')
+    parser.add_argument('-p', '--password', help='NSX ALB API Password')
+    parser.add_argument('-t', '--tenant', help='Tenant',
+                        default='admin')
+    parser.add_argument('-x', '--apiversion', help='NSX ALB API version')
+    parser.add_argument('-tx', '--tfversion', help='NSX ALB API version')
+    parser.add_argument('objecttype', help='Type of the object')
+    parser_n = parser.add_mutually_exclusive_group()
+    parser_n.add_argument('-s', '--search',
+                          help='Search for object names containing string')
+    parser_n.add_argument('-n', '--names',
+                          help='Comma-separated list of object names')
+    parser.add_argument('filename', help='Output .tf file for resources')
+
+    args = parser.parse_args()
+
+    if args:
+        # If not specified on the command-line, prompt the user for the
+        # controller IP address and/or password
+
+        controller = args.controller
+        user = args.user
+        password = args.password
+        tenant = args.tenant
+        api_version = args.apiversion
+        tf_version = args.tfversion
+        object_type = args.objecttype
+        object_names = args.names
+        object_search = args.search
+        output_fn = args.filename
+
+        while not controller:
+            controller = input('Controller:')
+
+        while not password:
+            password = getpass.getpass(f'Password for {user}@{controller}:')
+
+        if not api_version:
+            # Discover Controller's version if no API version specified
+
+            api = ApiSession.get_session(controller, user, password)
+            api_version = api.remote_api_version['Version']
+            api.delete_session()
+            print(f'Discovered Controller version {api_version}.')
+        api = ApiSession.get_session(controller, user, password,
+                                     api_version=api_version)
+
+        params = {'fields': 'uuid,name'}
+
+        if object_search:
+            params['search'] = f'(name,{object_search})'
+        elif object_names:
+            if ',' in object_names:
+                params['name.in'] = object_names
+            else:
+                params['name'] = object_names
+
+        matching_objects = api.get_objects_iter(object_type, tenant=tenant,
+                                                params=params)
+
+        print('Preparing environment', end='')
+
+        with TemporaryDirectory() as td:
+            with open(f'{td}/main.tf', mode='w') as tf_main:
+                tf_version = tf_version or api_version
+                tf_boilerplate = ['terraform {\n',
+                                  '  required_providers {\n',
+                                  '    avi = {\n',
+                                  '      source = "vmware/avi"\n',
+                                  f'      version = "{tf_version}"\n',
+                                  '    }\n',
+                                  '  }\n',
+                                  '}\n',
+                                  '\n',
+                                  'provider "avi" {\n',
+                                  f'  avi_username    = "{user}"\n',
+                                  f'  avi_tenant      = "{tenant}"\n',
+                                  f'  avi_password    = "{password}"\n',
+                                  f'  avi_controller  = "{controller}"\n',
+                                  f'  avi_version     = "{api_version}"\n'
+                                  '}\n',
+                                  '\n']
+                tf_main.writelines(tf_boilerplate)
+                resources = []
+                for object in matching_objects:
+                    print('.', end='')
+                    object_uuid = object['uuid']
+                    object_names = object['name']
+                    rs = f'resource "avi_{object_type}" "{object_uuid}" {{ }}\n'
+                    tf_main.write(rs)
+                    resources.append((object_uuid, object_names))
+                tf_main.flush()
+                print()
+
+            print(f'Initializing Terraform (vmware/avi {tf_version})...')
+
+            p = run(['terraform', f'-chdir={td}', 'init'], capture_output=True)
+
+            if p.returncode:
+                print(f'Error invoking terraform init:')
+                print(p.stderr.decode('UTF-8'))
+            else:
+                print('Importing resources...')
+                with open(output_fn, mode='wb') as output:
+                    for object_uuid, object_names in resources:
+                        print(f'Importing {object_uuid} ({object_names})')
+                        p = run(['terraform',
+                                f'-chdir={td}',
+                                 'import',
+                                 f'avi_{object_type}.{object_uuid}',
+                                 f'{object_uuid}'],
+                                capture_output=True)
+                        if p.returncode:
+                            print(f'Error invoking terraform import:')
+                            print(p.stderr.decode('UTF-8'))
+                            continue
+                        p = run(['terraform',
+                                f'-chdir={td}',
+                                 'state',
+                                 'show',
+                                 f'avi_{object_type}.{object_uuid} '],
+                                capture_output=True)
+                        if p.returncode:
+                            print(f'Error invoking terraform state show:')
+                            print(p.stderr.decode('UTF-8'))
+                            continue
+                        output.write(bytes(f'# {object_names}\n',
+                                           encoding='UTF-8'))
+                        output.write(p.stdout)
+                        output.write(b'\n\n')
+                print()
+                print(f'Resources have been written to {output_fn}')
+
+    else:
+        parser.print_help()
